fa203e9dd20811657cd7d6e8cbe5734fe890d631
evelynliu7 <144566499+evelynliu7@users.noreply.github.com>
2025-09-17 16:29:28 +0000

    3 elementary row operations as functions

--- Matrices_as_Lists_Med.py
+++ Matrices_as_Lists_Med.py
@@ -1,6 +1,16 @@
 import copy
 from tabulate import tabulate
 
+def AddRows(a, b):
+    print("add")
+
+def MultRow(a, scalar):
+    print("multiply")
+
+def SwapRows(a, b):
+    print("swap")
+
+
 m = int(input("Choose the number of rows in your matrix: "))
 n = int(input("Choose the number of columns in your matrix: "))
 
@@ -19,3 +29,5 @@ print("Enter 3 to swap two rows.")
 
 action = int(input("Enter your action: "))
 
+if(action == 1):
+    MultRow(1, 1)
