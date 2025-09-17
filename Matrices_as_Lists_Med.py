79308e48a81825164fdad27cbd56a884fa5513ae
evelynliu7 <144566499+evelynliu7@users.noreply.github.com>
2025-09-17 16:25:46 +0000

    added own py file

---
+++ Matrices_as_Lists_Med.py
@@ -0,0 +1,21 @@
+import copy
+from tabulate import tabulate
+
+m = int(input("Choose the number of rows in your matrix: "))
+n = int(input("Choose the number of columns in your matrix: "))
+
+M = []
+
+for i in range(m):
+    row = input("Enter row " + str((i+1)) + ": ")
+    M.append(list(row))
+
+print("Here is matrix:\n")
+print(tabulate(M))
+
+print("Enter 1 to multiply a row by a scalar.")
+print("Enter 2 to add a scalar of one row to another row.")
+print("Enter 3 to swap two rows.")
+
+action = int(input("Enter your action: "))
+
